10e6ee455444a56fa0f1083444b1f86a7328fb55
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-07-04 08:42:54 +0200

    :notebook:  IB actions added

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet var billText: UITextField!
+    @IBOutlet var zeroPctBtn: UIButton!
+    @IBOutlet var tenPctBtn: UIButton!
+    @IBOutlet var twentyPctBtn: UIButton!
+    @IBOutlet var splitNumberLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    @IBAction func stepperValueChanged(_ sender: Any) {
+    }
+    @IBAction func CalculatePressed(_ sender: Any) {
+    }
+    
+}
+
